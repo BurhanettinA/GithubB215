a7118236ce715d66d64892b807a0f88a3b281bef
Burhanettin Aydin <albora20@gmail.com>
2023-11-04 16:52:45 +0100

    1V1

---
+++ src/main/java/Class01.java
@@ -0,0 +1,13 @@
+public class Class01 {
+    public static void main(String[] args) {
+
+
+        //Version 1 icin kodlar
+
+        int x = 1;
+        int y = 2;
+        System.out.println(x+y);
+        System.out.println("Version 1 tamamlandi.");
+
+    }
+}

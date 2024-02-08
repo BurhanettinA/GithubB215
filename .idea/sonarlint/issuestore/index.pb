0bf6f192aff65472ca8f086720a645cfdb76bacc
burhanettin <burhanettin>
2024-02-08 11:11:39 +0100

    v2

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+J
+src/main/java/Class01.java,e\f\ef1da281708036f55bbb5fd2826484d571a69408
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+J
+src/main/java/class02.java,9\8\988b7c2c8dcd161f776f571ab2638ee2e384a084
---
+++ src/main/java/class02.java
@@ -0,0 +1,14 @@
+public class class02 {
+    public static void main(String[] args) {
+        // version 1 icin kodlar
+        int x = 1;
+        int y = 2;
+        System.out.println(x+y);
+        System.out.println("Version 1 tamamlandi");
+    }
+
+
+
+
+
+}
